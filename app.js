8f63a269232e1cfae89d7c4e29d35f85387c6a9e
SheikhAhmer <sheikhahmerali75@gmail.com>
2021-11-11 01:51:02 +0500

    some code

---
+++ app.js
@@ -0,0 +1,126 @@
+// import axios from "axios";
+
+import axios from "axios";
+// import { response } from "express";
+
+// import axios from "axios";
+
+// const url = process.env.PORT||3000;
+function post() {
+    let userName = document.getElementById("userName").value;
+    let email = document.getElementById("email").value;
+    let address = document.getElementById("address").value;
+
+    axios('https://crud-opperation.herokuapp.com/user', {
+        // method: 'post',
+        // url=url + '/',
+        userName: userName,
+        email: email,
+        address: address
+    })
+
+        // data: {
+
+        // },
+        .then((response) => {
+            // if (response.data.status === 200) {
+
+            alert(response.data)
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("address").value = "";
+            put();
+            // }
+        })
+        .catch((error) => {
+            console.log(error)
+            alert(error)
+
+        });
+
+}
+
+
+
+function get() {
+    axios.get("https://crud-opperation.herokuapp.com/users")
+        .then((response) => {
+            $html = '';
+            console.log(response);
+            var i = 0;
+            response.data.forEach((data) => {
+                if (data.userName != undefined) {
+                    $html += '<tr>';
+                    $html += '<td id="userName"' + i + '">' + data.userName + '</td>';
+                    $html += '<td id="email"' + i + '">' + data.email + '</td>';
+                    $html += '<td id="address"' + i + '">' + data.address + '</td>';
+                    $html += '<td><a href="javascript:void(0)" onclick="record(this);" id=' + i + '>'
+                    $html += '</tr>'
+                }
+                i++;
+            })
+            console.log($html);
+            console.log(document.getElementById('tblper'));
+            document.getElementById('tblper').innerHTML = $html;
+        })
+        .catch((error) => {
+            console(error);
+
+        })
+        .then((excicute) => {
+            console.log(excicute)
+        });
+}
+
+
+function record($obj) {
+    var id = $obj.getAttribute('id');
+    id = parseInt(id);
+    let userName = document.getElementById("userName" + id).innerHTML;
+    let email = document.getElementById("email" + id).innerHTML;
+    let address = document.getElementById("address" + id).innerHTML;
+
+    console.log(userName, id, document.getElementById('UserName : ' + id));
+
+    document.getElementById('userName').value = userName;
+    document.getElementById('email').value = email;
+    document.getElementById('address').value = address;
+}
+
+
+function update() {
+    let userName = document.getElementById("userName").value;
+    let email = document.getElementById("email").value;
+    let address = document.getElementById("address").value;
+    let id = document.getElementById("id").value;
+
+    axios.put("https://crud-opperation.herokuapp.com/user/" + id, {
+        userName: userName,
+        email: email,
+        address: address,
+    })
+        .then((response) => {
+            alert("user updated" + response);
+            get();
+        })
+        .catch((error) => {
+            console.log(error);
+            console.log(err);
+
+        });
+}
+function delete_data() {
+    let id = document.getElementById("id").value;
+    axios.delete("https://crud-opperation.herokuapp.com/user/" + id)
+        .then((response) => {
+        console.log(response);
+        alert(response.data)
+        get();
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        .then((excicute)=>{
+            console.log(excicute);
+        })
+}
---
+++ server.mjs
@@ -0,0 +1,106 @@
+import express from "express";
+import cors from "cors"
+// import axios from "axios";
+import morgan from "morgan";
+
+const app = express();
+// const axios = axios();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan());
+
+
+const port = process.env.PORT || 3000;
+
+
+let users = [];
+app.use((req,res,next)=>{
+    console.log("yae req ha user ki trf se"+req.body)
+    next();
+})
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+// app.post((req, res, next) => {
+    //     console.log("user is here", req.body)
+    //     res.send(req.body);
+//     next();
+// })
+
+
+// app.get('/users', (req, res) => {
+//     res.send(users)
+//   })
+
+
+app.get('/user:id', (req, res) => {
+    if (users[req.params.id]) {
+        res.send(users[req.params.id])
+    } else {
+        
+        res.send("user not found")
+    }
+    
+})
+app.post('/user', (req, res) => {
+    if (!req.body.userName || !req.body.email || !req.body.address) {
+        res.status(400).send("invalid data");
+    } else {
+        users.push({
+            userName: req.body.userName,
+            email: req.body.email,
+            address: req.body.address,
+
+        },
+        console.log("yae array ha"+users)
+        )
+
+        res.send("user created successfully")
+    }
+})
+app.put('user/:id', (req, res) => {
+    if (users[req.params.id]) {
+        if (req.body.userName) {
+            users[req.params.id].userName = req.body.userName;
+        } if (req.body.email) {
+            users[req.params.id].email = req.body.email;
+        }
+        //     if(req.body.userName){
+        //     users[req.params.id].userName=req.body.;
+        // }
+        if (req.body.address) {
+            users[req.params.id].address = address;
+
+        }
+        res.send(users[req.params.id])
+
+    }
+    else {
+        res.send("user not found")
+    }
+
+})
+
+app.delete('/user/:id', (req, res) => {
+
+    if (users[req.params.id]) {
+        users[req.params.id] === {};
+        res.send("userDeleted")
+    }else{
+        res.send("user not found")
+    }
+})
+
+//   app.get('/', (req, res) => {
+//     res.send('Hi I am a hello world Server program')
+//   })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+// axios.get("/user")
+//     .then((res) => {
+//         res.data.user
+//     })
